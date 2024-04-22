cb5de5d87956f5755d6cbd5e91dfdf67d45e56f7
sandeep2197 <s.sandeep2197@gmail.com>
2024-04-22 13:24:38 +0530

    added mail parser

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import { SMTPServer } from "smtp-server";
+import { MailParser } from "mailparser";
+
+const parser = new MailParser();
 
 const server = new SMTPServer({
   allowInsecureAuth: true,
@@ -16,10 +19,17 @@ const server = new SMTPServer({
     callback();
   },
   onData(stream, session, callback) {
-    stream.on("data", (data) => {
-      //console buffer data to String
-      console.log("data : ", data.toString());
-      callback();
+    stream.on("data", async (data) => {
+      parser.on("data", (data) => {
+        if (data.type === "text") {
+          console.log("Text Data: ", data.html);
+        }
+        if (data.type === "attachment") {
+          console.log("Attachment Data: ", data.filename);
+          data.content.pipe(process.stdout);
+          data.content.on("end", () => data.release());
+        }
+      });
     });
     stream.on("end", () => {
       console.log("stream end.");
